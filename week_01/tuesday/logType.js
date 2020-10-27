cbd9687c839777d6d3b20d068371c7cf153e02ba
Rens Pennings <renspennings@gmail.com>
2020-10-27 20:02:29 +0100

    finished logType

--- week_01/tuesday/logType.js
+++ week_01/tuesday/logType.js
@@ -1,29 +1,31 @@
 function logType(par) {
-    if (isNaN(par)) {
-        console.log("Not a Number!");
-    } else if (par === Array) {
+    if (Array.isArray(par)) {
         console.log("Array!");
     } else if (typeof par === "bigint") {
         console.log("bigint!");
-    } else if (typeof par === "number"){ 
-        console.log("number!");
     } else if (typeof par === "string") {
         console.log("string!");
     } else if (typeof par === "boolean") {
         console.log("boolean!");
     } else if (typeof par === "object") {
         console.log("Object!");
+    } else if (typeof par === "undefined") {
+        console.log("undefined!");
+    } else if (isNaN(par)) {
+        console.log("Not a Number!");
+    } else if (typeof par === "number"){ 
+        console.log("number!");
     } else if (typeof par === "function") {
         console.log("Function!");
-    } else if (typeof par === undefined) {
-        console.log("undefined!");
     } else {
         console.log("I have no idea!");
     }
 }
-var object = {};
+var object = NaN;
 
-logType([]);
+var arr = [];
+
+logType(3);
 
 
 //"null!"
