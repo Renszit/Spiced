5bc470b9c62848bdbf8c60394ecb8d0223b46acd
Rens Pennings <renspennings@gmail.com>
2020-10-28 08:10:35 +0100

    fixed NaN catching functions

--- week_01/tuesday/logType.js
+++ week_01/tuesday/logType.js
@@ -13,12 +13,12 @@ function logType(par) {
         console.log("Object!");
     } else if (typeof par === "undefined") {
         console.log("undefined!");
-    } else if (isNaN(par)) {
+    } else if (typeof par === "function") {
+        console.log("Function!");
+    } else if (Number.isNaN(par)) {
         console.log("Not a Number!");
     } else if (typeof par === "number"){ 
         console.log("number!");
-    } else if (typeof par === "function") {
-        console.log("Function!");
     } else {
         console.log("I have no idea!");
     }
