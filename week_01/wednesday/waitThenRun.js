889072084a8cc4cc401d9e875eecd5e8c2134e00
Rens Pennings <renspennings@gmail.com>
2020-10-28 14:10:32 +0100

    added code to waitThenRun

--- week_01/wednesday/waitThenRun.js
+++ week_01/wednesday/waitThenRun.js
@@ -7,5 +7,5 @@ waitThenRun(function() {
     console.log('Hello!');
     waitThenRun(function() {
         console.log('Goodbye!');
-    }); // logs 'Goodbye!' 1.5 seconds later
-}); // logs 'Hello!' 1.5 seconds later
+    }); 
+}); 
