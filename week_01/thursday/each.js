3093f94c34800d3657decf85b3c06238714ac422
Rens Pennings <renspennings@gmail.com>
2020-10-29 11:44:13 +0100

    added excercise code to each

---
+++ week_01/thursday/each.js
@@ -0,0 +1,16 @@
+function each(objOrArr, callback) {
+    
+}
+
+
+// excercise code:
+each({
+    a: 1,
+    b: 2
+}, function(val, name) {
+    console.log('The value of ' + name + ' is ' + val);
+}); // logs 'the value of a is 1' and 'the value of b is 2'
+
+each(['a', 'b'], function(val, idx) {
+    console.log('The value of item ' + idx + ' is ' + val);
+}); // logs 'the value of item 0 is a' and 'the value of item 1 is b'
