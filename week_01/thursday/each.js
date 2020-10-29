68165f7e4466769e7072e5c49bcc95cf4651c4ca
Rens Pennings <renspennings@gmail.com>
2020-10-29 15:23:43 +0100

    finished each.js

--- week_01/thursday/each.js
+++ week_01/thursday/each.js
@@ -1,16 +1,25 @@
-function each() {
+function each(objOrArray, callback) {
+    if (Array.isArray(objOrArray)) {
+        for (var i = 0; i < objOrArray.length; i++) {
+            callback(objOrArray[i], i);
+        }
+    } else {
+        for (var arg in objOrArray) {
+            callback(objOrArray[arg], arg);
+        }
+    }
 }
-
- 
-
 // excercise code:
-each({
-    a: 1,
-    b: 2
-}, function(val, name) {
-    console.log('The value of ' + name + ' is ' + val);
-}); // logs 'the value of a is 1' and 'the value of b is 2'
+each(
+    {
+        a: 1,
+        b: 2,
+    },
+    function (val, name) {
+        console.log("The value of " + name + " is " + val);
+    }
+); // logs 'the value of a is 1' and 'the value of b is 2'
 
-each(['a', 'b'], function(val, idx) {
-    console.log('The value of item ' + idx + ' is ' + val);
+each(["a", "b"], function (val, idx) {
+    console.log("The value of item " + idx + " is " + val);
 }); // logs 'the value of item 0 is a' and 'the value of item 1 is b'
