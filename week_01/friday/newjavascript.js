374a95caae022d38a9b7b254b19236fb18469d81
Rens Pennings <renspennings@gmail.com>
2020-10-30 13:49:12 +0100

    added code to excercise

---
+++ week_01/friday/newjavascript.js
@@ -0,0 +1,27 @@
+var x;
+
+var doublex;
+
+x = 205;
+
+function timesTwo(num){
+    return (num * 2);
+}
+
+doublex = timesTwo(x);
+
+var numbers;
+
+numbers = [doublex, x];
+
+//loop through numbers
+
+
+for (i=0; i < numbers.length; i++) {
+    console.log(numbers);
+}
+
+//lost me// got distracted.
+
+
+
