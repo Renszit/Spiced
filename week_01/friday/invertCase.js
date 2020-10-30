d25eb9746e8d8f1027ed9ecf7c142ce13327294e
Rens Pennings <renspennings@gmail.com>
2020-10-30 17:10:08 +0100

    invertCas not yet finished

---
+++ week_01/friday/invertCase.js
@@ -0,0 +1,24 @@
+function invertCase(string) {
+    
+}
+
+
+
+
+
+// function invertCase(str) {
+//     if (str == Number) {
+//         return str;
+//     } else {
+//         var newStr = "";
+//         for (var i = str.length - 1; i >= 0; i--) {
+//             newStr += str[i];
+//         }
+//         return newStr.toUpperCase();
+//     }
+// }
+
+// invertCase(jeEL);
+
+//excercise code
+// str == str.toUpperCase();
