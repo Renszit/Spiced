a5aec90279cb88ece7c0da442704f4f2800e16d8
Rens Pennings <renspennings@gmail.com>
2020-11-03 17:06:09 +0100

    finished boxinbox

--- week_02/Tuesday/4.boxinbox/script.js
+++ week_02/Tuesday/4.boxinbox/script.js
@@ -1,1 +1,28 @@
-console.log("testing");
+// console.log("testing");
+
+// // console.log("sanity check");
+
+var outerBox = document.getElementById("outerBox");
+var innerBox = document.getElementById("innerBox");
+
+// // random color
+function getRandomColorNumber() {
+    return Math.floor(Math.random() * 256);
+}
+function randomColor() {
+    var r = getRandomColorNumber();
+    var g = getRandomColorNumber();
+    var b = getRandomColorNumber();
+    return "rgb(" + r + "," + g + "," + b + ")";
+}
+
+outerBox.addEventListener("click", function () {
+    outerBox.style.backgroundColor = randomColor();
+});
+
+innerBox.addEventListener("click", function (event) {
+    event.stopPropagation();
+    innerBox.style.backgroundColor = randomColor();
+});
+
+
