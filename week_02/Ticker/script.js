7b4c0d22b31d9c5f4760cc5282c377195da95021
Rens Pennings <renspennings@gmail.com>
2020-11-03 18:48:11 +0100

    finished ticker and some work on getty

--- week_02/Ticker/script.js
+++ week_02/Ticker/script.js
@@ -1,7 +1,17 @@
 (function () {
+    // var req = requestAnimationFrame(moveBox);
     var tckr = document.getElementById("ticker");
     var lft = tckr.offsetLeft;
     var lnks = document.getElementsByTagName("a");
+    var anim;
+
+    tckr.addEventListener("mouseenter", function () {
+        cancelAnimationFrame(anim);
+    });
+
+    tckr.addEventListener("mouseleave", function () {
+        requestAnimationFrame(moveBox);
+    });
 
     function moveBox() {
         if (lft <= -1 * lnks[0].offsetWidth) {
@@ -11,8 +21,10 @@
         lft--;
         tckr.style.left = lft + "px";
         lnks[0].offsetWidth;
-        requestAnimationFrame(moveBox);
+        anim = requestAnimationFrame(moveBox);
     }
 
     moveBox();
 })();
+
+
