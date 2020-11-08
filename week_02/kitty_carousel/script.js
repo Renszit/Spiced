8d7a90ec8f10843da5b21e0521de9dce845fffb0
Rens Pennings <renspennings@gmail.com>
2020-11-08 22:25:40 +0100

    fixed the first part of kitty carousel

--- week_02/kitty_carousel/script.js
+++ week_02/kitty_carousel/script.js
@@ -1,43 +1,35 @@
 (function () {
     var kitties = document.querySelectorAll(".kitty-container img");
-    var dots = document.querySelectorAll(".dot");
+    var count = 0;
     var timer;
-    var isTransitioning = false;
 
-    //dot index
-    for (var i = 0; i < dots.length; i++) {
-        dots[i].addEventListener("click", function (e) {
-            for (var j = 0; j < dots.length; j++) {
-                if (dots[j] === e.target) {
-                    console.log("dot index: ", j);
-                }
-            }
-        });
-    }
+    function moveKitties(index) {
+        console.log("index:", index);
+        if (typeof index === "number") {
+            kitties[index].classList.add("onscreen");
+            kitties[count].classList.remove("onscreen");
+            return;
+        } else {
+            kitties[count].classList.remove("onscreen");
+            kitties[count].classList.add("offscreen-left");
 
-    //moveKitties function needs work
-    function moveKitties() {
-        kitties[0].classList.add("offscreen-left");
-        kitties[0].classList.remove("onscreen");
-        kitties[1].classList.add("onscreen");
-
-        setTimeout(moveKitties, 3000);
-        // move dots inside function
-    }
-
-    //initial function call
-
-    //checking if transition has stopped and calling moveKitties again
-    for (var k = 0; k < kitties.length; k++) {
-        kitties[k].addEventListener("transitionend", function (e) {
-            for (var j = 0; j < kitties.length; j++) {
+            document.addEventListener("transitionend", function (e) {
                 if (e.target.classList.value == "offscreen-left") {
-                    kitties[j].classList.remove("offscreen-left");
-                } 
+                    e.target.classList.remove("offscreen-left");
+                    timer = setTimeout(function () {
+                        moveKitties();
+                    }, 3000);
+                }
+            });
+
+            if (count < kitties.length - 1) {
+                count++;
+                kitties[count].classList.add("onscreen");
+            } else {
+                count = 0;
+                kitties[count].classList.add("onscreen");
             }
-            setTimeout(moveKitties, 3000);
-        });
+        }
     }
+    setTimeout(function () {moveKitties();},3000);
 })();
-
-
