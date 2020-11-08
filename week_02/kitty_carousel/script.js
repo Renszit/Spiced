3fbd17e0ee010073c9d9991648494d694765beeb
Rens Pennings <renspennings@gmail.com>
2020-11-08 22:40:08 +0100

    unfinished kitty carousel

--- week_02/kitty_carousel/script.js
+++ week_02/kitty_carousel/script.js
@@ -2,7 +2,26 @@
     var kitties = document.querySelectorAll(".kitty-container img");
     var count = 0;
     var timer;
+    var dots = document.querySelectorAll(".dot");
+    var isTransitioning = false;
 
+    // for (var i = 0; i < dots.length; i++) {
+    //     (function (dotIndex) {
+    //         dots[i].addEventListener("click", function (e) {
+    //             if (isTransitioning) {
+    //                 return;
+    //             }
+    //             // if (if the dot that was clicked corresponds with the kitty that's currently onscreen) {
+    //             // do nothing
+    //             // }
+    //             console.log("dotIndex: ", dotIndex);
+    //             clearTimeout(timer); // this will cancel a setTimout
+    //             moveKitties(dotIndex);
+    //         });
+    //     })(i); // DONT FORGET TO INVOKE YOUR IIFE
+    // }
+
+    //moveKitties
     function moveKitties(index) {
         console.log("index:", index);
         if (typeof index === "number") {
@@ -10,26 +29,31 @@
             kitties[count].classList.remove("onscreen");
             return;
         } else {
+            dots[count].classList.remove("on");
             kitties[count].classList.remove("onscreen");
             kitties[count].classList.add("offscreen-left");
-
-            document.addEventListener("transitionend", function (e) {
-                if (e.target.classList.value == "offscreen-left") {
-                    e.target.classList.remove("offscreen-left");
-                    timer = setTimeout(function () {
-                        moveKitties();
-                    }, 3000);
-                }
-            });
 
             if (count < kitties.length - 1) {
                 count++;
                 kitties[count].classList.add("onscreen");
+                dots[count].classList.add("on");
             } else {
                 count = 0;
                 kitties[count].classList.add("onscreen");
+                dots[count].classList.add("on");
             }
         }
     }
-    setTimeout(function () {moveKitties();},3000);
+    
+    timer = setTimeout(moveKitties, 3000);
+
+    document.addEventListener("transitionend", function (e) {
+        if (e.target.classList.value == "offscreen-left") {
+            e.target.classList.remove("offscreen-left");
+            dots[count].classList.remove("on");
+            timer = setTimeout(function () {
+                moveKitties();
+            }, 3000);
+        }
+    });
 })();
