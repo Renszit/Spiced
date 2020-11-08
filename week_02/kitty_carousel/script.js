1e8fdf94f067dbf9872c86ed3948de3f869cfe0d
Rens Pennings <renspennings@gmail.com>
2020-11-08 14:29:32 +0100

    figuring kitty carousel out

--- week_02/kitty_carousel/script.js
+++ week_02/kitty_carousel/script.js
@@ -1,53 +1,43 @@
-// (function () {
-//     var kitties = Array.from(document.querySelectorAll(".kitty-container img"));
-
-//     function moveKitties() {
-//         kitties[0].classList.add("offscreen-left");
-//         kitties[0].classList.remove("onscreen");
-//         kitties[1].classList.add("onscreen");
-//     }
-
-//     setTimeout(moveKitties, 3000);
-
-//     document.addEventListener("transitionend", function (e) {
-//         if (e.target.classList.value == "offscreen-left") {
-//             kitties[0].classList.remove("offscreen-left");
-//             kitties[kitties.length] = kitties[0];
-//             kitties.shift();
-//             setTimeout(moveKitties, 3000);
-//         }
-//     });
-// })();
-// solution above here does not seem ideal. Trying new stuff below:
-
 (function () {
     var kitties = document.querySelectorAll(".kitty-container img");
+    var dots = document.querySelectorAll(".dot");
+    var timer;
+    var isTransitioning = false;
 
+    //dot index
+    for (var i = 0; i < dots.length; i++) {
+        dots[i].addEventListener("click", function (e) {
+            for (var j = 0; j < dots.length; j++) {
+                if (dots[j] === e.target) {
+                    console.log("dot index: ", j);
+                }
+            }
+        });
+    }
+
+    //moveKitties function needs work
     function moveKitties() {
         kitties[0].classList.add("offscreen-left");
         kitties[0].classList.remove("onscreen");
         kitties[1].classList.add("onscreen");
+
+        setTimeout(moveKitties, 3000);
+        // move dots inside function
     }
 
-    setTimeout(moveKitties, 3000);
+    //initial function call
 
-    document.addEventListener("transitionend", function (e) {
-
-        if (e.target.classList.value == "offscreen-left") {
-            kitties[0].classList.remove("offscreen-left");
-            kitties[kitties.length] = kitties[0];
-            kitties.shift();
+    //checking if transition has stopped and calling moveKitties again
+    for (var k = 0; k < kitties.length; k++) {
+        kitties[k].addEventListener("transitionend", function (e) {
+            for (var j = 0; j < kitties.length; j++) {
+                if (e.target.classList.value == "offscreen-left") {
+                    kitties[j].classList.remove("offscreen-left");
+                } 
+            }
             setTimeout(moveKitties, 3000);
-        }
-    });
+        });
+    }
 })();
 
-// for (var i = 0; i < dots.length; i++) {
-//     dots[i].addEventListener("click", function (e) {
-//         for (var j = 0; j < dots.length; j++) {
-//             if (dots[j] === e.target) {
-//                 console.log("dot index: ", j);
-//             }
-//         }
-//     });
-// }
+
