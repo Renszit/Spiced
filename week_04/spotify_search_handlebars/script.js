70068b452a5d350e83327a250d2814b4c7e7b759
Rens Pennings <renspennings@gmail.com>
2020-11-18 12:06:22 +0100

    spotify search work in progress

--- week_04/spotify_search_handlebars/script.js
+++ week_04/spotify_search_handlebars/script.js
@@ -1,11 +1,29 @@
 (function () {
+    var defaultImg = "https://i.pinimg.com/originals/7a/ec/a5/7aeca525afa2209807c15da821b2f2c6.png";
     var moreButton = $("#moreButton");
+    var doc = $(document);
+    var win = $(window);
+    var infScroll = location.search.search(/\bscroll=infinite\b/) > -1;
     var nextUrl;
     var results;
+    var timer;
+
+    function getData (url) {
+        var q, type, moreWasClicked;
+
+    }
+
+
+
+
+
+
     $("#submit-button").on("click", function () {
         var userInput = $("input").val();
         var albumOrArtist = $("select").val();
 
+
+        
         $.ajax({
             method: "GET",
             url: "https://spicedify.herokuapp.com/spotify",
@@ -19,6 +37,10 @@
                 response = response.artists || response.albums;
                 var myHtml = "";
                 var redirection = "";
+
+                if (location.search.indexOf("scroll=infinite") > -1) {
+                    checkScrollPosition();
+                }
                 //html result
                 if (response.items.length > 0) {
                     $("#resultsFor").html(
@@ -37,7 +59,21 @@
                         imageUrl = response.items[i].images[0].url;
                     }
                     redirection = response.items[i].external_urls.spotify;
-                    myHtml += "<a href=" + redirection +">" +"<img src=" + imageUrl + ">" + "</a>" + "<a href=" + redirection +">" + "<div>" + response.items[i].name + "</div>" + "</a>" ;
+                    myHtml +=
+                        "<a href=" +
+                        redirection +
+                        ">" +
+                        "<img src=" +
+                        imageUrl +
+                        ">" +
+                        "</a>" +
+                        "<a href=" +
+                        redirection +
+                        ">" +
+                        "<div>" +
+                        response.items[i].name +
+                        "</div>" +
+                        "</a>";
 
                     $("#results-container").html(myHtml);
 
@@ -55,6 +91,26 @@
             }),
         });
     });
+
+    //check scroll position
+    function checkScrollPosition() {
+        setTimeout(function () {
+            if (
+                $(window).height() + $(document).scrollTop() ===
+                $(document).height()
+            ) {
+                moreButton.css({ visibility: "hidden" });
+                $.ajax({
+                    method: "GET",
+                    url: nextUrl,
+                    success: results,
+                });
+                console.log("at bottom of page!");
+            } else {
+                checkScrollPosition();
+            }
+        }, 500);
+    }
 
     moreButton.on("click", function () {
         $.ajax({
