2c9788b3ca4c7843bc1d7ce482e4037922281663
Rens Pennings <renspennings@gmail.com>
2020-11-18 20:20:29 +0100

    finished excercises 1-3, 4 needs work"

--- week_04/wednesday/4_2002/script.js
+++ week_04/wednesday/4_2002/script.js
@@ -1,13 +1,31 @@
-let getNameAndCountry = ({ name, country }) => [name, country];
-
-let getRelocatedCity = (city1, city2 = { country: "Germany" }) => {
-    let [, country] = getNameAndCountry(city2);
-    return {
-        ...city1,
-        country,
-    };
+var city1 = {
+    name: "Berlin",
+    country: "Germany",
+};
+var city2 = {
+    name: "Rotterdam",
+    country: "Netherlands",
 };
 
+// let getNameAndCountry = ({ name, country }) => [name, country];
 
-console.log(getRelocatedCity);
+function getNameAndCountry(object) {
+    return [object.name, object.country];
+}
 
+console.log(getNameAndCountry(city1));
+console.log(getNameAndCountry(city2));
+
+// let getRelocatedCity = (city1, city2 = { country: "Germany" }) => {
+//     let [, country] = getNameAndCountry(city2);
+//     return {
+//         ...city1,
+//         country,
+//     };
+// };
+
+function getRelocatedCity(obj1, obj2) {
+    
+}
+
+console.log(getRelocatedCity(city1));
