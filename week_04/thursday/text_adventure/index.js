2e13d449834301a28b3ce6217b45ed59a23c362f
Rens Pennings <renspennings@gmail.com>
2020-11-19 18:03:13 +0100

    finished text adventure

--- week_04/thursday/text_adventure/index.js
+++ week_04/thursday/text_adventure/index.js
@@ -8,38 +8,55 @@ const rl = readline.createInterface({
 });
 
 const story = {
-    q: "Welcome to The Land Of Wizards! Would you like to play?(yes/no)",
+    q: `It is the first day of the year 2021, you wake up after a long slumber with a hangover.
+Around you is no one. 
+"ah right.. Covid". 
+There are stale chips on the table. 
+"hm" you think "I ám hungry..." 
+what do you do? (eat/throw) `,
     answers: {
-        yes: {
+        eat: {
             q:
-                "You are alone in a dark forest and facing a fork in the road. Which direction do you turn? (left/right)",
+            `
+Uhm. Gross. You actually eat the chips. 
+They have been there since last year at least!
+Hm. Anyway, now that we have established what you are willing to do for this text adventure.. 
+The house is a real mess. You should do some cleaning. (clean/leave) `,
             answers: {
-                left: {
+                clean: {
                     q:
-                        "There's a scary wizard! He asks you a tough question. What's 1+1?",
+                        `
+You start cleaning the house, which is really disgusting. 
+What did you do last night? I know it has been a rough year but c'mon, 20(!) whiskey bottles?.
+This is not the way you should live your life.
+Sorry. I should not judge you, I am just a computer.
+You find one whiskey bottle which has not been finished. (drink/toss away)`,
                     answers: {
-                        2: "congratulations!",
+                        drink: "congratulations! You just started 2021 with a bang!",
                     },
                 },
-                right: "This was not the right choice. Goodbye!",
+                leave: `Or you should leave I guess? 
+If you are not going to take my suggestions I think it's better if we end this here. 
+Don't say you like text based stories and then just leave when you are able. 
+sigh.`,
             },
         },
-        no: "Alright then. Enjoy your day!",
+        throw: `For some reason you throw the chips against the wall.. have fun cleaning up dummy`,
     },
 };
 
 function askQuestion(storyObj) {
     if (storyObj.q === undefined) {
-        console.log(storyObj);
+        console.log(chalk.green(storyObj));
         rl.close();
     } else
         rl.question(chalk.inverse(storyObj.q), (answer) => {
             if (storyObj.answers[answer]) {
                 askQuestion(storyObj.answers[answer]);
             } else {
-                console.log(
-                    "ok.. not sure what you are saying here, let's try again."
-                );
+                (console.log(chalk.red(
+                    "ok.. not sure what you mean, let's try again."
+                )));
                 askQuestion(storyObj);
             }
         });
