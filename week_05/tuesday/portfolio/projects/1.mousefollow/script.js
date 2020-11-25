8fa4656bab31e390b714963e63c11b057cdd2501
Rens Pennings <renspennings@gmail.com>
2020-11-25 11:32:16 +0100

    Removed some projects from projects folder

--- week_05/tuesday/portfolio/projects/1.mousefollow/script.js
+++
@@ -1,7 +0,0 @@
-// console.log("sanity check");
-
-window.addEventListener("mousemove", function (event) {
-    var elem = document.getElementById("element");
-    elem.style.top = event.clientY - 50 + "px";
-    elem.style.left = event.clientX - 50 + "px";
-});
