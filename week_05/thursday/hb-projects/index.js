cac371684e39d6dbc4cde06a331a3bc3dcd0a913
Rens Pennings <renspennings@gmail.com>
2020-11-28 13:50:18 +0100

    working, but still working on styling

--- week_05/thursday/hb-projects/index.js
+++ week_05/thursday/hb-projects/index.js
@@ -12,9 +12,23 @@ app.use(express.static("./public"));
 app.get("/", (req, res) => {
     console.log("user is requesting GET / route");
     res.render("home", {
-        layout: "main", 
+        layout: "main",
+        home: true,
         projects,
     });
 });
 
-app.listen(8080, ()=> console.log("listening"));
+app.get("/projects/:project", (req, res) => {
+    const project = req.params.project;
+    const selectedProject = projects.find((item) => item.directory == project);
+    console.log("user is going to a project");
+    if (!selectedProject) {
+        return res.sendStatus(404);
+    }
+    res.render("description", {
+        projects,
+        selectedProject,
+    });
+});
+
+app.listen(8080, () => console.log("listening"));
